665d985463a33053fd31af6c5a7cdb8ed0062ed8
shayshahal <shay840@gmail.com>
2022-12-31 21:08:53 +0200

    initialization

---
+++ webpack.common.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: "./src/DOM/index.js",
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        title: "Weather App",
+        template: "/src/DOM/index.html",
+        filename: 'index.html'
+    }),
+    ],
+    output: {
+        filename: "[name][contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    }
+};
