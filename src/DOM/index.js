3fe7dd98e06dfff5b75f0957cbeb5b93f3fd3936
shayshahal <shay840@gmail.com>
2023-01-04 19:38:34 +0200

    complete overhaul

--- src/DOM/index.js
+++ src/DOM/index.js
@@ -1,112 +1,75 @@
-import { getCities, getCountries, getWeather} from "../logic/apiHandler";
+import { getWeather} from "../logic/apiHandler";
 import './style.css'
-import bg1 from './assets/bg0.jpg'
-import bg2 from './assets/bg1.jpg'
-import bg3 from './assets/bg2.jpg'
-const submits = document.querySelectorAll('.submit');
-const searches = document.querySelectorAll('input')
-const suggests = document.querySelectorAll('.suggest');
-const forms = document.querySelectorAll('form')
-let loc = {country: '', city:'', cCode:'', lat:null, lon:null};
-let fahrenheit = false;
-const bgs = [bg1, bg2, bg3]
-searches[0].addEventListener('input', debounce(fetchCountryList,1000));
 
-async function fetchCountryList()
-{
-    const list = await getCountries(searches[0].value)
-    console.log(list)
-    clearSuggest(0);
-    list.forEach(x=>addSuggestion(0,x.name, x.code));
-    if(list.length === 1) loc['country'] = list[0].code;
-}
-function debounce(func, waitFor) {
-    let timeout;
-    return (...args) => new Promise(resolve => {
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-        timeout = setTimeout(() => resolve(func(...args)), waitFor);
-    });
-}
-function addSuggestion(i, name, data)
+const submit = document.querySelector('#submit');
+const search = document.querySelector('input')
+const suggest = document.querySelector('#suggest');
+const form = document.querySelector('form')
+let fahrenheit = false;
+
+function addSuggestion(name, data)
 {
     const btn = document.createElement('button');
     console.log(name)
     btn.textContent = name;
     btn.type = 'button';
-    suggests[i].appendChild(btn);
+    btn.classList.add('btnsgst');
+    suggest.appendChild(btn);
     btn.addEventListener('mousedown', (e)=> {
-        searches[i].value = e.target.textContent;
-        clearSuggest(i)
-        if(i === 0)
-        {
-            loc['country'] = name;
-            loc['cCode'] = data;
-        }
-        else
-        {
-            loc['city'] = name;
-            loc['lat'] = data[0];
-            loc['lon'] = data[1];
-        }
+        search.value = e.target.textContent;
+        clearSuggest()
+        submit.click();
     })
 }
 
-function clearSuggest(i)
+function clearSuggest()
 {
-    while (suggests[i].firstChild) {
-        suggests[i].removeChild(suggests[i].firstChild);
+    while (suggest.firstChild) {
+        suggest.removeChild(suggest.firstChild);
     }
-    suggests[i].textContent = '';
+    suggest.textContent = '';
 }
-
-submits[0].addEventListener('click', initCitySearch)
-
-function initCitySearch()
-{
-    clearSuggest(0);
-    submits[0].disabled = true;
-    searches[0].disabled = true;
-    forms[1].style.display = 'flex';
-    searches[1].focus();
-    submits[1].addEventListener('click', presentWeather)
-}
+submit.addEventListener('click', presentWeather)
 async function presentWeather()
 {
-    clearSuggest(1)
-    const location = document.getElementById('location');
+    clearSuggest()
+    const countryText = document.getElementById('countryText');
+    const cityText = document.getElementById('cityText');
     const weather = document.getElementById('temp')
-    const res = await getWeather(loc['lat'], loc['lon']);
-    let temp = ~~(res.main.temp - 273.15) + '℃';
-    if(fahrenheit)
-        temp = ~~(temp*1.8)+32 + '℉';
-    location.textContent = (loc['country'] || 'Your') + (loc['city']||' Place');
-    weather.textContent = temp;
-    const img = document.querySelector('img');
-    img.src = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
-    const container = document.getElementById('container');
-    container.style.backgroundImage = `url(${bgs[~~(Math.random()*3)]})`
+    try{
+        const res = await getWeather(search.value);
+        let temp = ~~(res.main.temp - 273.15) + '℃';
+        if(fahrenheit)
+            temp = ~~(temp*1.8)+32 + '℉';
+        countryText.textContent = res.sys.country;
+        cityText.textContent = res.name;
+        weather.textContent = temp;
+        const img = document.querySelector('img');
+        img.src = `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
+    }
+    catch
+    {
+        weather.textContent='';
+        cityText.textContent = ''
+        countryText.textContent = 'Couldn\'t get weather';
+    }
 }
 
-searches[1].addEventListener('input', debounce(async()=>{
-    const list = await getCities(searches[1].value, loc['cCode']); 
-    console.log(list)
-    clearSuggest(1);
-    list.forEach(x=>addSuggestion(1,x.name, [x.latitude, x.longitude]));
-    if(list.length === 1) loc = [list[0].latitude,list[0].longitude];
-}, 1000));
+form.addEventListener('submit', (e)=>e.preventDefault());
 
-forms.forEach(f=> f.addEventListener('submit', (e)=>e.preventDefault()));
-
-searches.forEach((f,i)=> f.addEventListener('focusout', ()=>{
-    clearSuggest(i);
-    }));
-
-try{
-    presentWeather();
-}
-catch(error)
+form.addEventListener('focusin', ()=>
 {
-    console.error(error);
-}
+    form.classList.add('scale')
+})
+form.addEventListener('focusout', ()=>{
+    clearSuggest();
+    form.classList.remove('scale');
+    });
+
+const swtch = document.getElementById('switch');
+swtch.addEventListener('change', ()=>
+{
+    fahrenheit = swtch.checked;
+});
+
+submit.click();
--- src/logic/apiHandler.js
+++ src/logic/apiHandler.js
@@ -1,18 +1,14 @@
 const keys = {
     'weatherApi' : '519901a9b0492b434e86015cdf5f4227',
     'abstractapi': '1b07984699ad4daba476457930a2222f',
-    'rapid': 'b40340b672msh04e2a94b030e6fep10b861jsn9222dd58e2f3',
 };
-export async function getWeather(lat, lon)
+export async function getWeather(input)
 {
-    if(!lat && !lon)
+    if(!input)
     {
-        const location = await getVisitorLocation();
-        lat = location.latitude;
-        lon = location.longitude;
+        input = await getVisitorLocation();
     }
-    console.log(lat, lon)
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat.toFixed(2)}&lon=${lon.toFixed(2)}&appid=${keys['weatherApi']}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${keys['weatherApi']}`);
     const data = await response.json();
     console.log(data)
     return data;
@@ -22,33 +18,5 @@ export async function getVisitorLocation()
     const response = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${keys['abstractapi']}`);
     const data = await response.json();
     console.log(data)
-    return data;
-}
-export async function getCities(input, country)
-{
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': keys['rapid'],
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-        }
-    };
-    const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=5&countryIds=${country}&namePrefix=${input}`, options);
-    const data = await response.json();
-    console.log(data)
-    return data.data;
-}
-export async function getCountries(input)
-{
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': keys['rapid'],
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-        }
-    };
-    const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/countries?limit=5&namePrefix=${input}`, options);
-    const data = await response.json();
-    console.log(data)
-    return data.data;
+    return data.country;
 }
