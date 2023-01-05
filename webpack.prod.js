73e8d51fa799bd46a8b02b50e25d5a32f9f27479
shayshahal <shay840@gmail.com>
2023-01-05 04:03:47 +0200

    .

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
@@ -15,4 +16,7 @@ module.exports = merge(common, {
             },
         ],
     },
+    optimization: {
+        minimizer: [new CssMinimizerPlugin()]
+    }
 });
